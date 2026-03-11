347218d18ccf997c5853bee1ea7c8e3a6a1f3323
jdhurtadom-uni <jd.hurtadom@unimayor.edu.co>
2026-03-11 17:51:27 -0500

    Arma.java

--- Arma.java
+++ Arma.java
@@ -1,5 +1,9 @@
+/**
+ * Representa un arma que el personaje tiene equipado.
+ * Las armas incrementan permanentemente el daño base del personaje que la usa.
+ */
 public class Arma implements Objeto {
-    private static final int BONUS_DANIO = 10; // DR
+    private static final int BONUS_DANIO = 10; // Cantidad de daño extra que otorga
     private String nombre;
 
     public Arma(String nombre) { this.nombre = nombre; }
@@ -9,6 +13,7 @@ public class Arma implements Objeto {
 
     @Override
     public void usar(Personaje personaje) {
+        // Accede al método del personaje para subir su daño
         personaje.aumentarDanio(BONUS_DANIO);
         System.out.println(nombre + " equipada. Daño +" + BONUS_DANIO);
     }
