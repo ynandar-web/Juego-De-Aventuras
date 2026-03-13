1c19ce3f53ff1a1827d249fff31e756b2408af2d
David7777777777777 <david.orozco@unimayor.edu.co>
2026-03-12 21:34:32 -0500

    Update Pocion.java

--- Pocion.java
+++ Pocion.java
@@ -1,5 +1,8 @@
+/**
+ * Representa un objeto consumible que cura al personaje.
+ */
 public class Pocion implements Objeto {
-    private static final int VIDA_RESTAURADA = 25;
+    private static final int VIDA_RESTAURADA = 25; // Cantidad de vida que cura
     private String nombre;
 
     public Pocion(String nombre) { this.nombre = nombre; }
@@ -9,6 +12,7 @@ public class Pocion implements Objeto {
 
     @Override
     public void usar(Personaje personaje) {
+        // Llama al método del personaje para curarlo
         personaje.restaurarVida(VIDA_RESTAURADA);
         System.out.println(nombre + " usada. Vida +" + VIDA_RESTAURADA);
     }
